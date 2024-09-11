3822da2ab63171f74f38326600fe3bc1b8cabd57
Shubham <shubham.jha@tekditechnologies.com>
2024-09-12 01:09:44 +0530

    Added type in CSV and config for Login API

--- module/learningResources/helper.js
+++ module/learningResources/helper.js
@@ -6,86 +6,72 @@
  */
 
 /**
-    * LearningResourcesHelper
-    * @class
-*/
+ * LearningResourcesHelper
+ * @class
+ */
 
 module.exports = class LearningResourcesHelper {
+	/**
+	 * Extract learning resources from csv.
+	 * @method
+	 * @name extractLearningResourcesFromCsv
+	 * @param {Object} parsedCsvData - Parsed csv data.
+	 * @returns {Array} learning resources data.
+	 */
 
-    /**
-    * Extract learning resources from csv. 
-    * @method
-    * @name extractLearningResourcesFromCsv 
-    * @param {Object} parsedCsvData - Parsed csv data.
-    * @returns {Array} learning resources data. 
-   */
+	static extractLearningResourcesFromCsv(parsedCsvData) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				console.log(parsedCsvData)
+				let learningResources = []
 
-    static extractLearningResourcesFromCsv( parsedCsvData ) {
-        return new Promise(async (resolve, reject) => {
-            try {
+				for (let resourceCount = 1; resourceCount < 20; resourceCount++) {
+					let resource = 'learningResources' + resourceCount + '-'
+					let resourceName = resource + 'name'
+					let resourceLink = resource + 'link'
+					let resourceApp = resource + 'app'
+					let resourceId = resource + 'id'
+					let resourceType = resource + 'type'
 
-                let learningResources = [];
+					let resources = {}
 
-                for ( 
-                    let resourceCount = 1 ; 
-                    resourceCount < 20 ; 
-                    resourceCount++ 
-                ) {
-                    
-                    let resource = "learningResources" + resourceCount + "-";
-                    let resourceName = resource + "name";
-                    let resourceLink = resource + "link";
-                    let resourceApp = resource + "app";
-                    let resourceId = resource + "id";
+					if (parsedCsvData[resourceName] !== '' && parsedCsvData[resourceName] !== undefined) {
+						resources['name'] = parsedCsvData[resourceName]
+						delete parsedCsvData[resourceName]
+					}
 
-                    let resources = {};
-                    
-                    if( 
-                        parsedCsvData[resourceName] !== "" && 
-                        parsedCsvData[resourceName] !== undefined 
-                    ) {
-                        resources["name"] = parsedCsvData[resourceName];
-                        delete parsedCsvData[resourceName];
-                    }
-    
-                    if( 
-                        parsedCsvData[resourceLink] !== "" &&
-                        parsedCsvData[resourceLink] !== undefined 
-                    ) {
-                        resources["link"] = parsedCsvData[resourceLink];
-                        delete parsedCsvData[resourceLink];
-                    }
-                    
-                    if( 
-                        parsedCsvData[resourceApp] !== "" && 
-                        parsedCsvData[resourceApp] !== undefined ) {
-                        resources["app"] = parsedCsvData[resourceApp];
-                        delete parsedCsvData[resourceApp];
-                    }
+					if (parsedCsvData[resourceLink] !== '' && parsedCsvData[resourceLink] !== undefined) {
+						resources['link'] = parsedCsvData[resourceLink]
+						delete parsedCsvData[resourceLink]
+					}
 
-                    if( 
-                        parsedCsvData[resourceId] !== "" && 
-                        parsedCsvData[resourceId] !== undefined 
-                    ) {
-                        resources["id"] = parsedCsvData[resourceId];
-                        delete parsedCsvData[resourceId];
-                    } 
+					if (parsedCsvData[resourceApp] !== '' && parsedCsvData[resourceApp] !== undefined) {
+						resources['app'] = parsedCsvData[resourceApp]
+						delete parsedCsvData[resourceApp]
+					}
 
-                    if( Object.keys(resources).length > 0 ) {
-                        learningResources.push(resources);
-                    }
-                }
+					if (parsedCsvData[resourceId] !== '' && parsedCsvData[resourceId] !== undefined) {
+						resources['id'] = parsedCsvData[resourceId]
+						delete parsedCsvData[resourceId]
+					}
 
-                return resolve({
-                    success: true,
-                    message : CONSTANTS.apiResponses.DUPLICATE_PROJECT_TEMPLATES_CREATED,
-                    data : learningResources
-                })
+					if (parsedCsvData[resourceType] !== '' && parsedCsvData[resourceType] !== undefined) {
+						resources['type'] = parsedCsvData[resourceType]
+						delete parsedCsvData[resourceType]
+					}
 
-            } catch (error) {
-                return reject(error);
-            }
-        })
-    }
-    
+					if (Object.keys(resources).length > 0) {
+						learningResources.push(resources)
+					}
+				}
+				return resolve({
+					success: true,
+					message: CONSTANTS.apiResponses.DUPLICATE_PROJECT_TEMPLATES_CREATED,
+					data: learningResources,
+				})
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
 }

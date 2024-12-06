c17823224a066367cb4ec1e48d94f74d0beec948
praveenKDass <praveendass@shikshalokam.org>
2024-12-06 18:32:53 +0530

    kafka_topic value changes

--- generics/kafka/producers.js
+++ generics/kafka/producers.js
@@ -15,7 +15,7 @@ const projectSubmissionTopic =
 const userProjectActivityTopic =
 	process.env.USER_ACTIVITY_TOPIC && process.env.USER_ACTIVITY_TOPIC != 'OFF'
 		? process.env.USER_ACTIVITY_TOPIC
-		: 'sl-user-project-submission-dev'
+		: 'dev-user-activities'
 
 /**
  * Push improvement projects to kafka.

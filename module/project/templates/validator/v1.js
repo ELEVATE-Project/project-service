671374e3872de103d0cc1c4aee292a019e21b39d
priyanka-TL <priyankapratheepan@gmail.com>
2024-12-24 09:59:47 +0530

    validation updated

--- module/project/templates/validator/v1.js
+++ module/project/templates/validator/v1.js
@@ -15,6 +15,12 @@ module.exports = (req) => {
 		publishTemplateAndTask: function () {
 			req.checkBody('data').exists().withMessage('required project template data')
 			req.checkBody('callBackUrl').exists().withMessage('required callback URL')
+			req.checkBody('data.title').notEmpty().withMessage('title is required and cannot be empty')
+			req.checkBody('data.categories').notEmpty().withMessage('categories is required and cannot be empty')
+			req.checkBody('data.recommended_for').notEmpty().withMessage('recommended_for is required and cannot be empty')
+			req.checkBody('data.tasks').notEmpty().withMessage('tasks is required and cannot be empty')
+			req.checkBody('data.id').notEmpty().withMessage('id is required and cannot be empty')
+			req.checkBody('data.user_id').notEmpty().withMessage('user_id is required and cannot be empty')
 		}
 	}
 

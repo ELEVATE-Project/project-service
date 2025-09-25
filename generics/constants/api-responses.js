52ef6e1692458174c761678ecc268c684c6b6e68
borkarsaish65 <borkarsaish65@gmail.com>
2025-09-25 18:59:08 +0530

    savepoint

--- generics/constants/api-responses.js
+++ generics/constants/api-responses.js
@@ -296,4 +296,5 @@ module.exports = {
 	FAILED_TO_ADD_ENTITY: 'Failed to add entity to the project',
 	ENTITY_ADDED_TO_PROJECT_SUCCESSFULLY: 'Entity added to project successfully',
 	PROJECT_ENTITIES_UPDATE_NOT_ALLOWED: 'Project entities updation not allowed',
+	INVALID_MAPPING_DATA: 'Invalid mapping data',
 }
